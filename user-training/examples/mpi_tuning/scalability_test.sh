5bc45ac87a7e28ff6b1f9b735477c0e56bc3e132
Jordi Blasco <jordi.blasco@gmail.com>
2017-08-18 15:42:37 +1200

    fix output file redirection in scalability_test.sh

--- user-training/examples/mpi_tuning/scalability_test.sh
+++ user-training/examples/mpi_tuning/scalability_test.sh
@@ -7,6 +7,6 @@ ml intel/2017a
 cp $HOME/snow-labs/user-training/examples/Cardiac_demo/mesh_mid* $SCRATCH_DIR/
 cp $HOME/snow-labs/user-training/examples/Cardiac_demo/setup_mid.txt $SCRATCH_DIR/
 cd $SCRATCH_DIR
-srun $HOME/snow-labs/user-training/examples/Cardiac_demo/build_non-instrumented/heart_demo -m ./mesh_mid -s ./setup_mid.txt -t 50
+srun $HOME/snow-labs/user-training/examples/Cardiac_demo/build_non-instrumented/heart_demo -m ./mesh_mid -s ./setup_mid.txt -t 50 | tee output.txt
 mkdir -p $HOME/snow-labs/user-training/OUT/
 echo "$SLURM_NTASKS x $SLURM_CPUS_PER_TASK : $(grep 'wall time:' output.txt)" >> $HOME/snow-labs/user-training/OUT/benchmark-results.txt
